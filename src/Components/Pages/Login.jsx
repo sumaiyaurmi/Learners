6c7cd254713284d06ecac42b51d1d36da1e2ecfe
Sumaiya Urmi <akthersumaiya689@gmail.com>
2024-05-21 13:47:52 +0600

    customized theme

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
 };
 
     return (
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-center my-4 items-center'>
         <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
         <div
             className='hidden bg-cover rounded-2xl bg-center lg:block lg:w-1/2'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
     extend: {
       fontFamily: {
-        poppins: "'Poppins', 'sans-serif' "
-      }
+        poppins: "'Poppins', 'sans-serif' ",
+      },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],}
-
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "luxury"],
+  },
+};
